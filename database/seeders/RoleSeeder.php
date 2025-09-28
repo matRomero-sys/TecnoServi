7115d227792ca97b68b9f34e6daf810177d8a914
matias <cuentadeestudioa1@gmail.com>
2025-09-28 12:16:09 -0300

    Seeder de Roles y Permisos

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create(['name' => 'ver registros']);
+        Permission::create(['name' => 'crear registros']);
+        Permission::create(['name' => 'editar registros']);
+        Permission::create(['name' => 'borrar registros']);
+
+
+        $admin = Role::create(['name' => 'admin']);
+        $empleado = Role::create(['name' => 'empleado']);
+    
+        $admin->givePermissionTo(Permission::all());
+        $empleado->givePermissionTo(['ver registros']);
+    }
+
+    
+}
