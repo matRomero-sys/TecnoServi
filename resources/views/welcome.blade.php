19bffc45cac22ffb5048658dbb102b40db172033
VirginiaIbarra <virginiaibarra743@gmail.com>
2025-10-01 19:38:14 -0300

    mini cambio en el welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,6 @@
         <a href="{{route('empleado.index')}}">Empleado</a>
         <a href="{{route('grupo-trabajo.index')}}">Grupo Trabajo</a>
         <a href="{{ route('orden-trabajo.index') }}">Orden de Trabajo</a>
-        <a href="">5</a>
-        <a href="">6</a>
-        <a href="">7</a>
     
     </div>
 
