f43eb1e18e455c7440c1224b595a6443b917a65e
matias <cuentadeestudioa1@gmail.com>
2025-10-13 17:15:51 -0300

    Cambio en estilos y vista de store de Empleado

--- resources/views/empleado/store.blade.php
+++ resources/views/empleado/store.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 
-    <form action="{{route('empleado.store')}}" method="POST">
+    <form action="{{route('empleado.store')}}" method="POST" class="form">
         @csrf
 
         @foreach ($camposForm as $campo => $type)
         @if (!in_array($campo, ['rendimiento', 'id_grupo_trabajo']))
-        <label for="{{$campo}}">{{implode(' ', array_map('ucfirst',explode('_', $campo)))}}</label>
-        <input type="{{$type}}" id="{{$campo}}" name="{{$campo}}">
-        <br>
+        <div>
+            <label for="{{$campo}}">{{implode(' ', array_map('ucfirst',explode('_', $campo)))}}</label>
+            <input type="{{$type}}" id="{{$campo}}" name="{{$campo}}">
+        </div>
         @endif
         @endforeach
 
